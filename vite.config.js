874b01fecfa6d55c74da96e8b63f1ab5ac7f2e4d
Piotr Kielak <p.kielak@outlook.com>
2026-01-08 19:40:10 +0100

    deployment-related fixes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     loader: "ts",
     include: /\.ts?$/,
   },
-  base: "/",
+  base: process.env.VITE_BASE_PATH || "/handmarking/",
   build: {
     rollupOptions: {
       input: "index.html",
